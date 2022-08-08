72f6cfed22e27a98a01cd5a818276e339f185737
cuihaoran <1054346513@qq.com>
2022-08-08 11:49:02 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@
  * @Description: 页面/组件/功能的描述
  * @FilePath: /vue3-ts-cms/src/main.ts
  */
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount('#app');
+createApp(App).mount('#app')
