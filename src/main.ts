3855f4c47ccdf531838ee00f03affe305e6747c5
cuihaoran <1054346513@qq.com>
2022-08-08 15:51:29 +0800

    feat(element-plus): 全量集成element-plus

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@
  * @FilePath: /vue3-ts-cms/src/main.ts
  */
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -13,4 +15,6 @@ import store from './store'
 const app = createApp(App)
 app.use(router)
 app.use(store)
+// 全局注册element-plus
+app.use(ElementPlus)
 app.mount('#app')
