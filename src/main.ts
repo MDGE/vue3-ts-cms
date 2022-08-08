d14b553c750737fc9f57fea30ac371704d0ff45e
cuihaoran <1054346513@qq.com>
2022-08-08 11:35:01 +0800

    测试husky

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/*
+ * @Author: 崔浩然
+ * @Email：cuihaoran@hualala.com
+ * @Date: 2022-08-08 09:35:45
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /vue3-ts-cms/src/main.ts
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
